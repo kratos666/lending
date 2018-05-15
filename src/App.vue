e1f4d77dca84ddd6f14834d282fca7ceee8eb9ac
Kratos <brutius666@gmail.com>
2018-05-15 08:36:51 +0300

    1

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-app>
+		<v-content>
+			<v-container fluid>
+				<step v-show="step1"></step>
+				<step v-show="step2"></step>
+				<step v-show="step3"></step>
+			</v-container>
+		</v-content>
+	</v-app>
+</template>
+
+<script>
+	import step from "./components/step.vue"
+	export default {
+		components: {
+			step
+		}
+	}
+</script>
---
+++ src/components/step.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<v-btn color="success" @click="changeStep(step1, step2)">Success</v-btn>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				step1: true,
+				step2: false,
+				step3: false
+			}
+		},
+		methods: {
+			changeStep: function(s1, s2) {
+				debugger;
+				this.s1 = false;
+				this.s2 = true;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
+
+
